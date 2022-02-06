eed283232b70f9a4037683bb61ed2a418016f50b
Anthony Iritano <81189148+antiritano@users.noreply.github.com>
2022-02-06 07:36:37 -0500

    Missing links

--- src/components/MostRecentVert.js
+++ src/components/MostRecentVert.js
@@ -5,7 +5,7 @@ const callouts = [
       imageSrc:
         'https://cdn.cloudflare.steamstatic.com/steam/apps/601150/header.jpg?t=1637363400',
       imageAlt: '',
-      href: '#',
+      href: '/reviews/Devil-May-Cry-5',
     },
     {
       name: 'Review',
@@ -13,7 +13,7 @@ const callouts = [
       imageSrc:
         'https://cdn.cloudflare.steamstatic.com/steam/apps/1196590/header.jpg?t=1640118086',
       imageAlt: '',
-      href: '#',
+      href: '/reviews/Resident-Evil-Village',
     },
     {
       name: 'Review',
@@ -21,7 +21,7 @@ const callouts = [
       imageSrc:
         'https://cdn.cloudflare.steamstatic.com/steam/apps/1446780/capsule_616x353.jpg?t=1640223337',
       imageAlt: '',
-      href: '#',
+      href: '/reviews/Monster-Hunter-Rise',
     },
     {
       name: 'Blog Post',
@@ -29,10 +29,10 @@ const callouts = [
       imageSrc:
         'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/7643695e-37e5-4fa7-8117-7caa9fe1d1ab/dcpyzj3-7f77aad0-5826-47fa-a145-d9478a9b3bb5.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzc2NDM2OTVlLTM3ZTUtNGZhNy04MTE3LTdjYWE5ZmUxZDFhYlwvZGNweXpqMy03Zjc3YWFkMC01ODI2LTQ3ZmEtYTE0NS1kOTQ3OGE5YjNiYjUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.gnzld7nUWtvfxVkDg42s3PePVAQ_RIrRnJlVTEWsWqo',
       imageAlt: '',
-      href: '#',
+      href: '/reviews/Dark-souls',
     },
   ];
-  
+
   export default function MostRecentVert() {
     return (
       <div>
@@ -41,7 +41,7 @@ const callouts = [
             <h2 className="text-2xl font-extrabold text-gray-900">
               Recent Articles
             </h2>
-  
+
             <div className="mt-6 space-y-12 lg:space-y-0 ">
               {callouts.map((callout) => (
                 <div key={callout.name} className="group relative">
@@ -69,4 +69,3 @@ const callouts = [
       </div>
     );
   }
-  
